7f55e764f46195736983d03ee3043960b30dc944
Toro <aatn1321@gmail.com>
2024-03-04 14:58:36 -0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://XxtbmfxX.github.io',
+    site: 'https://t0r0-2024-portfolio.netlify.app',
 });
